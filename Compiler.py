fead7b4f461b7a12a113f652bb4b9a7a9acf4438
RocketMaker69 <81818793+RocketMaker69@users.noreply.github.com>
2021-08-17 23:00:34 +0300

    Update Compiler.py

--- Compiler.py
+++ Compiler.py
@@ -1,28 +1,39 @@
 from emitter import Emitter
 
 # initialize emitter
-e = Emitter()
+e = Emitter("/storage/emulated/0/Download/out.py")
+s = {}
 
 def Compile(path):
+    print("PlanetScript Compiler")
     with open(path, "r") as f:
         e.HeaderLine(f"def main():\n")
         for l in f:
             if "=" in l:
                 # A variable declaration is the same in both PlanetScript and Python.
                 # No changes needed, so we can simply add the line to the file.
-                e.Line(f"    {l}")
+                e.Line(f"    {l}\n")
+                n = l[:l.find("=")-1]
+                v = l[l.find("=")+3:-2]
+                s[n] = v
 
             if "Print" in l:
-                a = l[l.find("t")+2:-1]
-                if "\t" not in l:
-                    e.Line(f"    print(f\"{a}\")\n")
+                a = l[l.find("t")+3:-1]
+                if "$" not in l:
+                    if "    " not in l:
+                        e.Line(f"    print(f\"{a}, end='')\n")
+                    else:
+                        e.Line(f"        print(f\"{a}, end='')\n")
                 else:
-                    e.Line(f"        print(f\"{a}\")\n")
+                	if "    " not in l:
+                		e.Line(f"    print({n}, end='')\n")
+                	else:
+                		e.Line(f"        print({n}, end='')\n")
 
             if "Input" in l:
                 a = l[l.find("t")+2:]
-                if "\t" not in l:
-                    e.Line(f"    {a} = input(\"\")\n")
+                if "    " not in l:
+                    e.Line(f"    {a.rstrip()} = input(\"\")\n")
                 else:
                     e.Line(f"        {a} = input(\"\")\n")
 
@@ -31,12 +42,17 @@ def Compile(path):
                 e.Line(f"    if {c}:\n")
 
             if "For" in l:
-                c = l[3:l.find(":")]
+                c = l[4:l.find(":")]
                 e.Line(f"    for {c}:\n")
 
             if "While" in l:
-                c = l[3:l.find(":")]
+                c = l[6:l.find(":")]
                 e.Line(f"    while {c}:\n")
+            if l == "\n":
+            	e.Line("\n")
+            if "#" in l:
+            	e.Line(f"    {l}")
     
-    e.Line("return 0\n")
+    e.Line("    return 0\n")
     e.Line("\nmain()")
+    print("Compiling finished...")
