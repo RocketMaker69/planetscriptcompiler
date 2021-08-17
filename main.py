bff7f02a2c2f9018d9bff17a397018cd83c9bae5
RocketMaker69 <81818793+RocketMaker69@users.noreply.github.com>
2021-08-17 19:40:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 from emitter import Emitter
-import sys
 
 # initialize emitter
 e = Emitter()
 
-with open(sys.argv[1], "r") as f:
-    for l in f:
-        if "=" in l:
-            # A variable declaration is the same in both PlanetScript and Python.
-            # No changes needed, so we can simply add the line to the file.
-            e.Line(l)
-        if "Print" in l:
-            a = l[l.find("t")+2:-1]
-            if "\t" not in l:
-                e.Line(f"print(f\"{a}\")\n")
-            else:
-                e.Line(f"    print(f\"{a}\")\n")
-        if "Input" in l:
-            a = l[l.find("t")+2:]
-            if "\t" not in l:
-                e.Line(f"{a} = input(\"\")\n")
-            else:
-                e.Line(f"    {a} = input(\"\")\n")
-        if "If" in l:
-            c = l[3:l.find(":")]
-            e.Line(f"if {c}:\n")
-        if "For" in l:
-            c = l[3:l.find(":")]
-            e.Line(f"for {c}:\n")
-        if "While" in l:
-            c = l[3:l.find(":")]
-            e.Line(f"while {c}:\n")
+def Compile(path):
+    with open(path, "r") as f:
+        for l in f:
+            if "=" in l:
+                # A variable declaration is the same in both PlanetScript and Python.
+                # No changes needed, so we can simply add the line to the file.
+                e.Line(l)
+            if "Print" in l:
+                a = l[l.find("t")+2:-1]
+                if "\t" not in l:
+                    e.Line(f"print(f\"{a}\")\n")
+                else:
+                    e.Line(f"    print(f\"{a}\")\n")
+            if "Input" in l:
+                a = l[l.find("t")+2:]
+                if "\t" not in l:
+                    e.Line(f"{a} = input(\"\")\n")
+                else:
+                    e.Line(f"    {a} = input(\"\")\n")
+            if "If" in l:
+                c = l[3:l.find(":")]
+                e.Line(f"if {c}:\n")
+            if "For" in l:
+                c = l[3:l.find(":")]
+                e.Line(f"for {c}:\n")
+            if "While" in l:
+                c = l[3:l.find(":")]
+                e.Line(f"while {c}:\n")
+    
+    return f
