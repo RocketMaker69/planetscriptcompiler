0ba81a4077b6841a9276025e868f8f41c5f16658
RocketMaker69 <81818793+RocketMaker69@users.noreply.github.com>
2021-08-17 17:55:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from emitter import Emitter
+import sys
 
 # initialize emitter
 e = Emitter()
@@ -7,4 +8,17 @@ e.Line("#include <stdio.h>;")
 e.Line("")
 e.Line("int main(void) {")
 
-match
+with open(sys.argv[1], "r") as f:
+    for l in f:
+        if "=" in l:
+            n = l[:l.find("=")]
+            v = l[l.find("=")+3:-1]
+            e.Line(f"    float {n};\n")
+            e.Line(f"    {n} = {v};\n")
+        if "Print" in l:
+            a = l[6:-1]
+            e.Line(f"    printf({a});\n")
+        if "Input" in l:
+            a = l[6:-1]
+            e.Line(f"    scanf(\"%f\", &{a});\n")
+    e.Line(f"    return 0")
