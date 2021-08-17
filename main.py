c6bf16de731542551b9c49ee1a38756f73612d7c
RocketMaker69 <81818793+RocketMaker69@users.noreply.github.com>
2021-08-17 18:48:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,17 @@ with open(sys.argv[1], "r") as f:
             v = l[l.find("=")+3:-1]
             e.Line(f"{n} = {v};\n")
         if "Print" in l:
-            a = l[6:-1]
-            e.Line(f"print(f\"{a}\")\n")
+            a = l[l.find("t")+2:-1]
+            if "\t" not in l:
+                e.Line(f"print(f\"{a}\")\n")
+            else:
+                e.Line(f"    print(f\"{a}\")\n")
         if "Input" in l:
-            a = l[5:]
-            e.Line(f"{a} = input(\"\")\n")
+            a = l[l.find("t")+2:]
+            if "\t" not in l:
+                e.Line(f"{a} = input(\"\")\n")
+            else:
+                e.Line(f"    {a} = input(\"\")\n")
+        if "If" in l:
+            c = l[3:l.find(":")]
+            e.Line(f"if {c}:\n")
