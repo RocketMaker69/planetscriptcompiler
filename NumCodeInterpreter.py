341e8c4220188d04ca409d1cf25c0736ab9ab076
RocketMaker69 <81818793+RocketMaker69@users.noreply.github.com>
2021-08-18 13:01:19 +0300

    Create NumCodeInterpreter.py

---
+++ NumCodeInterpreter.py
@@ -0,0 +1,29 @@
+class Interpreter:
+    def __init__(self):
+        self.s = []
+        self.e = {}
+
+    def Load(self, o):
+        self.s.append(o)
+
+    def SV(self, vn):
+        vv = self.s.pop()
+        self.e[vn] = vv
+
+    def LV(vn):
+        vv = self.e[vn]
+        return vv
+
+    def Run(self, code):
+        Instructions = code["instructions"]
+        Data = code["data"]
+        for step in Instructions:
+            i, a = step
+            if i == "LOAD":
+                self.Load(Data[a])
+            elif i == "STORE VAR":
+                self.SV(Data[a])
+            elif i == "LOAD VAR":
+                self.LV(Data[a])
+            elif i == "PRINT":
+                print(Data[a])
