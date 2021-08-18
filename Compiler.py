a9df15b3f4d81a58f6e0809ae3e24c2dbfd4d724
RocketMaker69 <81818793+RocketMaker69@users.noreply.github.com>
2021-08-18 14:24:18 +0300

    Update Compiler.py

--- Compiler.py
+++ Compiler.py
@@ -1,15 +1,16 @@
-def Compile(file):
-    newCode = {
-        "instructions": [],
-        "data"        : []
-    }
-    f = open(file, "w")
-    for l in f:
-        if "Print" in l:
-            arg = l[5:]
-            newCode["data"].append(arg)
-            newCode["instructions"].append(("PRINT", newCode["data"].index(arg)))
-        if "Input" in l:
-            arg = l[5:]
-            newCode["data"].append(arg)
-            newCode["instructions"].append(("INPUT", newCode["data"].index(arg)))
+class Compiler:
+    def __init__(self, f):
+        self.f = f
+
+    def Compile():
+        newCode = {"instructions": [],"data": []}
+        f = open(self.f, "w")
+        for l in f:
+            if "Print" in l:
+                arg = l[5:]
+                newCode["data"].append(arg)
+                newCode["instructions"].append(("PRINT", newCode["data"].index(arg)))
+            if "Input" in l:
+                arg = l[5:]
+                newCode["data"].append(arg)
+                newCode["instructions"].append(("INPUT", newCode["data"].index(arg)))
