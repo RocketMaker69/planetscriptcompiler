7fba822fc7901abff6f0c1a51053e7bb77d28300
RocketMaker69 <81818793+RocketMaker69@users.noreply.github.com>
2021-08-18 14:27:41 +0300

    Update Shell.py

--- Shell.py
+++ Shell.py
@@ -1,5 +1,12 @@
 from Compiler import Compiler
 from Interpreter import Interpreter
 
+f = input("Open path: ")
+
 i = Interpreter()
-c = Compiler()
+c = Compiler(f)
+
+code = c.Compile()
+
+i.Run(code)
+
